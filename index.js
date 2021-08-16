bdee2cbc7c1c3692cb9f3c673731e08c345f7e0f
mccleskeyc <mccleskeyc@gmail.com>
2021-08-16 18:16:31 -0400

    add even listener to display color

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 //check to see if the input from the user is a valid hex color
+const hexInput = document.getElementById('hexInput');
+const inputColor = document.getElementById('inputColor');
+
+hexInput.addEventListener('keyup', () => {
+    const hex = hexInput.value;
+    if(!isValidHex(hex)) return;
+    
+    const strippedHex = hex.replace('#', '');
+    inputColor.style.backgroundColor = "#" + strippedHex;
+})
+
 const isValidHex = (hex) => {
     if(!hex) return false;
 
